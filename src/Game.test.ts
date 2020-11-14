452b863b313653f0ae3f67a9ebe0740715addb1f
Mark Steve Samson <hello@marksteve.com>
2020-11-14 22:39:51 +0800

    Implement play comparison

---
+++ src/Game.test.ts
@@ -0,0 +1,56 @@
+import { Play } from './Game'
+
+// const gt = (c1, c2) => Play.fromString(c1).value() > Play.fromString(c2).value()
+const gt = (c1, c2) => {
+  return Play.fromString(c1).value() > Play.fromString(c2).value()
+}
+
+test('singles', () => {
+  expect(gt('2C', '9D')).toBeTruthy()
+  expect(gt('3D', '3C')).toBeTruthy()
+})
+
+test('pairs', () => {
+  expect(gt('2C 2D', '3S 3H')).toBeTruthy()
+  expect(gt('9C 9D', '3S 3H')).toBeTruthy()
+  expect(gt('3C 3D', '3S 3H')).toBeTruthy()
+})
+
+test('trio', () => {
+  expect(gt('2C 2D 2S', '3S 3H 3D')).toBeTruthy()
+  expect(gt('9C 9D 9H', '3S 3H 3D')).toBeTruthy()
+})
+
+test('straightFlush', () => {
+  expect(gt('2D 3D 4D 5D 6D', 'AD 2D 3D 4D 5D')).toBeTruthy()
+  expect(gt('3D 4D 5D 6D 7D', '2D 3D 4D 5D 6D')).toBeTruthy()
+  // This time the high card is the Ace.
+  expect(gt('AD KD QD JD TD', '9D TD JD QD KD')).toBeTruthy()
+})
+
+test('straight', () => {
+  // The high card in these cases are not the ace or two.
+  expect(gt('2C 3D 4D 5D 6D', 'AC 2D 3D 4D 5D')).toBeTruthy()
+  expect(gt('3C 4D 5D 6D 7D', '2C 3D 4D 5D 6D')).toBeTruthy()
+  // This time the high card is the Ace.
+  expect(gt('AC KD QD JD TD', '9C TD JD QD KD')).toBeTruthy()
+  // High card rank tied, break by suit.
+  expect(gt('AS KH QH JH TH', 'AC KD QD JD TD')).toBeTruthy()
+})
+
+test('quadro', () => {
+  expect(gt('9C 9S 9H 9D 4C', '3C 3S 3H 3D 2D')).toBeTruthy()
+  expect(gt('KC KS KH KD TC', '9C 9S 9H 9D 4D')).toBeTruthy()
+})
+
+test('fullHouse', () => {
+  expect(gt('9C 9S 9H 4D 4C', '3C 3S 3H 2C 2D')).toBeTruthy()
+  expect(gt('4C 4S 4H TD TC', '3C 3S 3H 4C 4D')).toBeTruthy()
+})
+
+test('flush', () => {
+  // Suit is more important than high card.
+  expect(gt('9D KD 8D JD TD', 'AC 2C 3C 4C 6C')).toBeTruthy()
+  // If suit is tied, then high card is used as breaker.
+  expect(gt('9D 2D 3D 4D 6D', 'AD KD 8D JD TD')).toBeTruthy()
+})
--- src/Game.ts
+++ src/Game.ts
@@ -1,11 +1,168 @@
 import { Game } from 'boardgame.io'
+import { ascending } from 'd3-array'
 
-export const TicTacToe: Game = {
-  setup: () => ({ cells: Array(9).fill(null) }),
+const RANKS = '3456789TJQKA2'
+const SUITS = 'CSHD'
 
-  moves: {
-    clickCell: (G, ctx, id) => {
-      G.cells[id] = ctx.currentPlayer
-    },
-  },
+class Card {
+  rank: string
+  suit: string
+
+  constructor(rank: string, suit: string) {
+    this.rank = rank
+    this.suit = suit
+  }
+
+  value() {
+    return RANKS.indexOf(this.rank) * 4 + SUITS.indexOf(this.suit)
+  }
+
+  static fromString(s: string) {
+    const [rank, suit] = s
+    return new Card(rank, suit)
+  }
+
+  static newDeck() {
+    const d: Card[] = []
+    for (const rank of RANKS) {
+      for (const suit of SUITS) {
+        d.push(new Card(rank, suit))
+      }
+    }
+    return d
+  }
+
+  static lowest = Card.fromString('3C')
+}
+
+export class Play {
+  cards: Card[]
+
+  constructor(cards: Card[]) {
+    this.cards = cards
+    this.cards.sort((a, b) => (a.value() > b.value() ? 1 : -1))
+  }
+
+  isSameRank(cards: Card[]) {
+    return cards[0].rank === cards[cards.length - 1].rank
+  }
+
+  straight() {
+    const num = (card: Card) => {
+      const r = RANKS.indexOf(card.rank)
+      return (r + 2) % 13
+    }
+    const numSet = new Set(this.cards.map(num))
+    if (numSet.size === 5) {
+      const nums = Array.from(numSet)
+      nums.sort(ascending)
+      if (nums[0] + 4 === nums[4]) {
+        // Use the value of the highest (by num) card.
+        const card = this.cards.find((c) => num(c) === nums[4])
+        return card.value()
+      } else if (nums[0] === 0) {
+        // Let Ace be a high card.
+        nums[0] = 13
+        nums.sort(ascending)
+        if (nums[0] + 4 === nums[4]) {
+          const card = this.cards.find((c) => num(c) === 0)
+          return card.value()
+        }
+      }
+    }
+  }
+
+  flush() {
+    if (this.cards.every((c) => c.suit === this.cards[0].suit)) {
+      return (
+        SUITS.indexOf(this.cards[4].suit) * 13 +
+        RANKS.indexOf(this.cards[4].rank)
+      )
+    }
+  }
+
+  quadro() {
+    if (
+      this.isSameRank(this.cards.slice(0, 4)) ||
+      this.isSameRank(this.cards.slice(1, 5))
+    ) {
+      return this.cards[1].value()
+    }
+  }
+
+  fullHouse() {
+    if (
+      this.isSameRank(this.cards.slice(0, 2)) ||
+      this.isSameRank(this.cards.slice(2, 5))
+    ) {
+      return this.cards[2].value()
+    } else if (
+      this.isSameRank(this.cards.slice(0, 3)) &&
+      this.isSameRank(this.cards.slice(3, 5))
+    ) {
+      return this.cards[2].value()
+    }
+  }
+
+  value() {
+    switch (this.cards.length) {
+      case 1:
+        return this.cards[0].value()
+      case 2:
+        if (this.isSameRank(this.cards)) {
+          return this.cards[1].value()
+        } else {
+          throw new Error("Pair doesn't match")
+        }
+      case 3:
+        if (this.isSameRank(this.cards)) {
+          return this.cards[0].value()
+        } else {
+          throw new Error("Trio doesn't match")
+        }
+      case 5:
+        const [combi, val] = (() => {
+          let val = this.straight()
+          if (val) {
+            if (this.flush()) {
+              return [Combi.StraightFlush, val]
+            } else {
+              return [Combi.Straight, val]
+            }
+          } else {
+            if ((val = this.quadro())) {
+              return [Combi.Quadro, val]
+            } else if ((val = this.fullHouse())) {
+              return [Combi.FullHouse, val]
+            } else if ((val = this.flush())) {
+              return [Combi.Flush, val]
+            } else {
+              return [Combi.None, 0]
+            }
+          }
+        })()
+        if (combi === Combi.None) {
+          throw new Error('Invalid 5-card combination')
+        } else {
+          return combi * 1000 + val
+        }
+    }
+  }
+
+  static fromString(s: string) {
+    return new Play(s.split(/\s+/).map((c) => Card.fromString(c)))
+  }
+}
+
+enum Combi {
+  None,
+  Straight,
+  Flush,
+  FullHouse,
+  Quadro,
+  StraightFlush,
+}
+
+export const PusoyDos: Game = {
+  setup: (ctx) => ({}),
 }
