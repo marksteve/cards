5be022796f3574b0b6baa2214fb2d231763c18d4
Mark Steve Samson <hello@marksteve.com>
2020-11-30 15:20:35 +0800

    Fix static serving

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,9 @@
 import { Server } from 'boardgame.io/server'
-import path from 'path'
 import serve from 'koa-static'
 import { PusoyDos } from './Game'
 
 const server = Server({ games: [PusoyDos] })
 const PORT = parseInt(process.env.PORT || '8000', 10)
 
-const frontEndAppBuildPath = path.resolve(__dirname, './build')
-server.app.use(serve(frontEndAppBuildPath))
-
-server.run(PORT, () => {
-  server.app.use(
-    async (ctx, next) =>
-      await serve(frontEndAppBuildPath)(
-        Object.assign(ctx, { path: 'index.html' }),
-        next
-      )
-  )
-})
+server.app.use(serve('./build'))
+server.run(PORT)
