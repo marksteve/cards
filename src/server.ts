9333bdcdba25d59f55ed9523117c6b04affefd55
Mark Steve Samson <hello@marksteve.com>
2020-12-09 23:28:32 +0800

    Turn into an SPA

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import { Server } from 'boardgame.io/server'
 import serve from 'koa-static'
+import send from 'koa-send'
 import { PusoyDos } from './Game'
 
 const server = Server({ games: [PusoyDos] })
 const PORT = parseInt(process.env.PORT || '8000', 10)
 
 server.app.use(serve('./build'))
+server.app.use(async (ctx, next) => {
+  await next();
+  if (ctx.response.status === 404) {
+    await send(ctx, './build/index.html')
+  }
+})
 server.run(PORT)
